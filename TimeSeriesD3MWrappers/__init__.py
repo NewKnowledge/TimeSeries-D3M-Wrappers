4348de264d4fe027d64cbc77d13e49bd32445ff6
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-02-06 11:32:38 -0600

    Update __init__.py

--- TimeSeriesD3MWrappers/__init__.py
+++ TimeSeriesD3MWrappers/__init__.py
@@ -1,6 +1,6 @@
 from TimeSeriesD3MWrappers.Parrot import Parrot
 from TimeSeriesD3MWrappers.Shallot import Shallot
-from TimeSeriesD3MWrappers.Sloth import Storc
+from TimeSeriesD3MWrappers.Storc import Storc
 from TimeSeriesD3MWrappers.timeseries_loader import TimeSeriesLoaderPrimitive
 
 __version__ = '1.0.0'
