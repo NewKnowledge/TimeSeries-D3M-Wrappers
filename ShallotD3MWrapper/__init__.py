9548d6f07ddbdeeca720db34d8e302860730d525
jlgleason <jeffrey.gleason.3@gmail.com>
2019-02-06 11:20:51 -0600

    condense into one directory

--- ShallotD3MWrapper/__init__.py
+++
@@ -1,9 +0,0 @@
-from ShallotD3MWrapper.Shallot import Shallot
-from ShallotD3MWrapper.timeseries_loader import TimeSeriesLoaderPrimitive
-
-__version__ = '1.0.1'
-
-__all__ = [
-    "Shallot",
-    "TimeSeriesLoaderPrimitive",
-]
--- SlothD3MWrapper/__init__.py
+++
@@ -1,9 +0,0 @@
-from SlothD3MWrapper.Storc import Storc
-from SlothD3MWrapper.timeseries_loader import TimeSeriesLoaderPrimitive
-
-__version__ = '2.0.2'
-
-__all__ = [
-           "Storc", 
-           "TimeSeriesLoaderPrimitive",
-           ]
