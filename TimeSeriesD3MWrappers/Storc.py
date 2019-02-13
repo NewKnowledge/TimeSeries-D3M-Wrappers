521d27f40f09d922b5c0637e737e7c8395c4e295
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-02-13 15:54:52 -0600

    import params class

--- TimeSeriesD3MWrappers/Storc.py
+++ TimeSeriesD3MWrappers/Storc.py
@@ -10,7 +10,7 @@ from d3m.primitive_interfaces.base import PrimitiveBase, CallResult
 
 from d3m import container, utils
 from d3m.container import DataFrame as d3m_DataFrame
-from d3m.metadata import hyperparams, base as metadata_base
+from d3m.metadata import hyperparams, base as metadata_base, params
 from common_primitives import utils as utils_cp, dataset_to_dataframe as DatasetToDataFrame
 
 from .timeseries_loader import TimeSeriesLoaderPrimitive
