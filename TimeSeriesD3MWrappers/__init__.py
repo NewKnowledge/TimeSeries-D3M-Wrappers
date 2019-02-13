fefe6a0e861374ff105e5b0c54a863d027eade90
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-02-13 15:30:54 -0600

    bump version

--- TimeSeriesD3MWrappers/__init__.py
+++ TimeSeriesD3MWrappers/__init__.py
@@ -3,7 +3,7 @@ from TimeSeriesD3MWrappers.Shallot import Shallot
 from TimeSeriesD3MWrappers.Storc import Storc
 from TimeSeriesD3MWrappers.timeseries_loader import TimeSeriesLoaderPrimitive
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 __all__ = [
            "Parrot", 
