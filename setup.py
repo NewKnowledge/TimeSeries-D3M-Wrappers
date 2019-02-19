e8b421a5f09f5349d8bb7f62aa314a135ad7fbec
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-02-19 13:41:02 -0600

    update entry point for Sloth, add entry points for kanine, hdbscan, bump version

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from distutils.core import setup
 
 setup(name='TimeSeriesD3MWrappers',
-    version='1.0.1',
-    description='Three wrappesr for interacting with New Knowledge time series tool Sloth',
+    version='1.0.2',
+    description='Five wrappers for interacting with New Knowledge time series tool Sloth',
     packages=['TimeSeriesD3MWrappers'],
     install_requires=["typing",
                       "Sloth==2.0.5"],
     dependency_links=[
-        "git+https://github.com/NewKnowledge/sloth@feb174c78b57667348e5b66213d542fbfe0797b3#egg=Sloth-2.0.5"
+        "git+https://github.com/NewKnowledge/sloth@eeca19594f2922487e21357fba6d15488331f5e8#egg=Sloth-2.0.5"
     ],
     entry_points = {
         'd3m.primitives': [
-            'time_series_segmentation.cluster.Sloth = TimeSeriesD3MWrappers:Storc',
+            'clustering.kmeans.Sloth = TimeSeriesD3MWrappers:Storc',
+            'clustering.time_series_clustering.Hdbscan = TimeSeriesD3MWrappers:Hdbscan',
             'time_series_classification.shapelet_learning.Shallot = TimeSeriesD3MWrappers:Shallot',
-            'time_series_forecasting.arima.Parrot = TimeSeriesD3MWrappers:Parrot'
+            'time_series_classification.k_neighbors.Kanine = TimeSeriesD3MWrappers:Kanine',
+            'time_series_forecasting.arima.Parrot = TimeSeriesD3MWrappers:Parrot',
         ],
     },
 )
