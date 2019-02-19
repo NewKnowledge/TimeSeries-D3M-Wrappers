7c0b80cc359c699b5ac5889c6d2f92437a0cc48d
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-02-19 13:37:55 -0600

    bump version, add kanine and hdbcan to initializations

--- TimeSeriesD3MWrappers/__init__.py
+++ TimeSeriesD3MWrappers/__init__.py
@@ -2,12 +2,16 @@ from TimeSeriesD3MWrappers.Parrot import Parrot
 from TimeSeriesD3MWrappers.Shallot import Shallot
 from TimeSeriesD3MWrappers.Storc import Storc
 from TimeSeriesD3MWrappers.timeseries_loader import TimeSeriesLoaderPrimitive
+from TimeSeriesD3MWrappers.Kanine import Kanine
+from TimeSeriesD3MWrappers.Hdbscan import Hdbcan
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 __all__ = [
            "Parrot", 
            "Shallot", 
            "Storc",
-           "timeseries_loader"
+           "timeseries_loader", 
+           "Kanine",
+           "Hdbscan"
            ]
