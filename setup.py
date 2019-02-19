c6d7dd0b3613b685549909761f4824ecb73b0ca1
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-02-19 15:12:17 -0600

    update entry point of hdbscan

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='TimeSeriesD3MWrappers',
     entry_points = {
         'd3m.primitives': [
             'clustering.kmeans.Sloth = TimeSeriesD3MWrappers:Storc',
-            'clustering.time_series_clustering.Hdbscan = TimeSeriesD3MWrappers:Hdbscan',
+            'clustering.hdbscan.Hdbscan = TimeSeriesD3MWrappers:Hdbscan',
             'time_series_classification.shapelet_learning.Shallot = TimeSeriesD3MWrappers:Shallot',
             'time_series_classification.k_neighbors.Kanine = TimeSeriesD3MWrappers:Kanine',
             'time_series_forecasting.arima.Parrot = TimeSeriesD3MWrappers:Parrot',
