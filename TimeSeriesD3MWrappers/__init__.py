9e9055bcdb1afe31ec0232f3c29f325044009249
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-02-20 14:08:52 -0600

    fix import typo

--- TimeSeriesD3MWrappers/__init__.py
+++ TimeSeriesD3MWrappers/__init__.py
@@ -3,7 +3,7 @@ from TimeSeriesD3MWrappers.Shallot import Shallot
 from TimeSeriesD3MWrappers.Storc import Storc
 from TimeSeriesD3MWrappers.timeseries_loader import TimeSeriesLoaderPrimitive
 from TimeSeriesD3MWrappers.Kanine import Kanine
-from TimeSeriesD3MWrappers.Hdbscan import Hdbcan
+from TimeSeriesD3MWrappers.Hdbscan import Hdbscan
 
 __version__ = '1.0.2'
 
