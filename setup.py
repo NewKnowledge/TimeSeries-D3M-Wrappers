4c639b1dbdeb3229d79050cc8cc1cdbc09b9f6b8
Jeffrey Gleason <jeffrey.gleason@newknowledge.io>
2019-03-20 12:24:35 -0500

    correct timeseriesloader package name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='TimeSeriesD3MWrappers',
     packages=['TimeSeriesD3MWrappers'],
     install_requires=["typing",
                       "Sloth==2.0.5",
-                      "distil-timeseries-loader"],
+                      "DistilTimeSeriesLoader==0.1.0"],
     dependency_links=[
         "git+https://github.com/NewKnowledge/sloth@fd86004a67965065cf1687f9d756c2ed7493d1a9#egg=Sloth-2.0.5",
         "git+https://github.com/uncharted-distil/distil-timeseries-loader@f64d97f39d42a74476529883a8cf1ecddfec4c87#egg=DistilTimeSeriesLoader"
