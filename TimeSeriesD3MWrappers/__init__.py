ad27004ee546ffec0536c7f686eb8544a5490934
Jeffrey Gleason <jeffrey.gleason@newknowledge.io>
2019-03-21 13:11:20 -0500

    delete timeseriesloader glue primitive

--- TimeSeriesD3MWrappers/__init__.py
+++ TimeSeriesD3MWrappers/__init__.py
@@ -1,7 +1,6 @@
 from TimeSeriesD3MWrappers.Parrot import Parrot
 from TimeSeriesD3MWrappers.Shallot import Shallot
 from TimeSeriesD3MWrappers.Storc import Storc
-from TimeSeriesD3MWrappers.timeseries_loader import TimeSeriesLoaderPrimitive
 from TimeSeriesD3MWrappers.Kanine import Kanine
 from TimeSeriesD3MWrappers.Hdbscan import Hdbscan
 
@@ -11,7 +10,6 @@ __all__ = [
            "Parrot", 
            "Shallot", 
            "Storc",
-           "timeseries_loader", 
            "Kanine",
            "Hdbscan"
            ]
