c0ec678f7889af4eb196833b80310a09d82f94c2
jgleason <jeffrey.gleason@newknowledge.io>
2019-04-10 14:13:08 -0500

    add cython install to setup

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from distutils.core import setup
+from setuptools import setup
+from setuptools.command.install import install as InstallCommand
+
+class Install(InstallCommand):
+    """ Customized setuptools install command which uses pip. """
+
+    def run(self, *args, **kwargs):
+        import pip3
+        pip3.main(['install', 'Cython'])
+        InstallCommand.run(self, *args, **kwargs)
 
 setup(name='TimeSeriesD3MWrappers',
     version='1.0.3',
     description='Five wrappers for interacting with New Knowledge time series tool Sloth',
     packages=['TimeSeriesD3MWrappers'],
+    cmdclass={'install': Install},
     install_requires=["typing",
                       "numpy",
-                      "Cython",
                       "Sloth @ git+https://github.com/NewKnowledge/sloth@3f527314445bfdc8197ed40b279bd74016d77c1b#egg=Sloth-2.0.6",
                       "DistilTimeSeriesLoader @ git+https://github.com/uncharted-distil/distil-timeseries-loader@b781b140ec1328939e7bf6005251b2145d9ca20a#egg=DistilTimeSeriesLoader-0.1.2",
                       ],
