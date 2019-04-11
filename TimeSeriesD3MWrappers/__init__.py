6a336ea54d04f1a59490c4848ff62a022956a5d3
jgleason <jeffrey.gleason@newknowledge.io>
2019-04-11 17:18:11 -0500

    add VAR to init file

--- TimeSeriesD3MWrappers/__init__.py
+++ TimeSeriesD3MWrappers/__init__.py
@@ -3,6 +3,7 @@ from TimeSeriesD3MWrappers.Shallot import Shallot
 from TimeSeriesD3MWrappers.Storc import Storc
 from TimeSeriesD3MWrappers.Kanine import Kanine
 from TimeSeriesD3MWrappers.Hdbscan import Hdbscan
+from TimeSeriesD3MWrappers.VAR import VAR
 from TimeSeriesD3MWrappers.timeseries_formatter import TimeSeriesFormatterPrimitive
 
 __version__ = '1.0.2'
@@ -13,5 +14,6 @@ __all__ = [
            "Storc",
            "Kanine",
            "Hdbscan",
+           "VAR",
            "timeseries_formatter"
            ]
