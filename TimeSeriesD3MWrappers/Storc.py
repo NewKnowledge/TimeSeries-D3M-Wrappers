1a14380b5bb662c7b41dba68c094cbfe5b613f13
jgleason <jeffrey.gleason@newknowledge.io>
2019-04-11 17:01:41 -0500

    local timeseriesformatter import

--- TimeSeriesD3MWrappers/Storc.py
+++ TimeSeriesD3MWrappers/Storc.py
@@ -13,7 +13,7 @@ from d3m.container import DataFrame as d3m_DataFrame
 from d3m.metadata import hyperparams, base as metadata_base, params
 from common_primitives import utils as utils_cp, dataset_to_dataframe as DatasetToDataFrame
 
-from timeseries_formatter import TimeSeriesFormatterPrimitive
+from .timeseries_formatter import TimeSeriesFormatterPrimitive
 
 __author__ = 'Distil'
 __version__ = '2.0.3'
