9840763c8f11c1a83d7874fe6d400b45400068bb
jgleason <jeffrey.gleason@newknowledge.io>
2019-04-11 17:15:18 -0500

    latest VAR primitive

--- TimeSeriesD3MWrappers/VAR.py
+++ TimeSeriesD3MWrappers/VAR.py
@@ -315,5 +315,5 @@ if __name__ == '__main__':
     var.fit()
     test_dataset = container.Dataset.load('file:///datasets/seed_datasets_current/LL1_736_stock_market/TEST/dataset_TEST/datasetDoc.json')
     results = var.produce(inputs = d3m_DataFrame(ds2df_client.produce(inputs = test_dataset).value))
-    #print(results.value)
-    results.value.to_csv('preds.csv', index=False)
+    print(results.value)
+    #results.value.to_csv('preds.csv', index=False)
