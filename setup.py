43f4605545a5562e5c16a36a5cf937502e21884a
jgleason <jeffrey.gleason@newknowledge.io>
2019-04-30 12:52:16 -0500

    KNN distance metric change in Sloth

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='TimeSeriesD3MWrappers',
     packages=['TimeSeriesD3MWrappers'],
     install_requires=["typing",
                       "numpy == 1.15.4",
-                      "Sloth @ git+https://github.com/NewKnowledge/sloth@acba16b3123a1ba1a05f32fcbeed1cca2cb843bb#egg=Sloth-2.0.5",
+                      "Sloth @ git+https://github.com/NewKnowledge/sloth@9c3753c6c5068470ec5e9d7340a274fdcc49cd4f#egg=Sloth-2.0.5",
                       ],
     entry_points = {
         'd3m.primitives': [
