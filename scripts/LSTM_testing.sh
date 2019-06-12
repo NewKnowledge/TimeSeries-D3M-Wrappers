2467297f6ea431bb0a5bbff6619fa0e1170dc2e4
jgleason <jeffrey.gleason@newknowledge.io>
2019-06-12 17:27:25 +0000

    add latest LSTM auto script

--- scripts/LSTM_testing.sh
+++ scripts/LSTM_testing.sh
@@ -1,17 +1,20 @@
 #!/bin/bash  
 
-Datasets=('LL1_Adiac' 'LL1_ArrowHead' '66_chlorineConcentration' 'LL1_CinC_ECG_torso' 'LL1_Cricket_Y' 'LL1_ECG200' 'LL1_ElectricDevices' 'LL1_FISH' 'LL1_FaceFour' 'LL1_FordA' 'LL1_HandOutlines' 'LL1_Haptics' 'LL1_ItalyPowerDemand' 'LL1_Meat' 'LL1_OSULeaf'
-cd /primitives/v2019.6.7/Distil/d3m.primitives.time_series_classification.convolutional_neural_net.LSTM_FCN/1.0.0/pipelines
-mkdir test_pipeline
+Datasets=('LL1_Adiac')
+# 'LL1_ArrowHead' '66_chlorineConcentration' 'LL1_CinC_ECG_torso' 'LL1_Cricket_Y' 'LL1_ECG200' 'LL1_ElectricDevices' 'LL1_FISH' 'LL1_FaceFour' 'LL1_FordA' 'LL1_HandOutlines' 'LL1_Haptics' 'LL1_ItalyPowerDemand' 'LL1_Meat' 'LL1_OSULeaf')
+#cd /primitives/v2019.6.7/Distil/d3m.primitives.time_series_classification.convolutional_neural_net.LSTM_FCN/1.0.0/pipelines
+cd ../TimeSeriesD3MWrappers/pipelines/
+#mkdir test_pipeline
+cd test_pipeline
 
 # create text file to record scores and timing information
 touch scores.txt
-echo "DATASET, F1 SCORE, EXECUTION TIME" > scores.txt
-cd test_pipeline
+echo "DATASET, F1 SCORE, EXECUTION TIME" >> scores.txt
+
 for i in "${Datasets[@]}"; do
 
   # generate and save pipeline + metafile
-  python3 "LSTM_FCN_pipeline_$i.py" $i
+  python3 "../LSTM_FCN_pipeline_$i.py" $i
   cp * ../
 
   # test and score pipeline
@@ -21,9 +24,8 @@ for i in "${Datasets[@]}"; do
   runtime=$((end-start))
 
   # save information
-  echo "\n$i, " > scores.txt
   IFS=, read col1 score col3 col4 < <(tail -n1 scores.csv)
-  echo "$score, $runtime" > scores.txt
+  echo "$i, $score, $runtime" >> scores.txt
   
   # cleanup temporary file
   rm *.meta
