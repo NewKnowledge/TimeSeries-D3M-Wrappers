24a7cb982f08e500463bf3c19bcbab4a14eaf711
jgleason <jeffrey.gleason@newknowledge.io>
2019-06-12 11:39:43 -0400

    add LSTM pipeline shell script

--- scripts/LSTM_testing.sh
+++ scripts/LSTM_testing.sh
@@ -3,11 +3,30 @@
 Datasets=('LL1_Adiac' 'LL1_ArrowHead' '66_chlorineConcentration' 'LL1_CinC_ECG_torso' 'LL1_Cricket_Y' 'LL1_ECG200' 'LL1_ElectricDevices' 'LL1_FISH' 'LL1_FaceFour' 'LL1_FordA' 'LL1_HandOutlines' 'LL1_Haptics' 'LL1_ItalyPowerDemand' 'LL1_Meat' 'LL1_OSULeaf'
 cd /primitives/v2019.6.7/Distil/d3m.primitives.time_series_classification.convolutional_neural_net.LSTM_FCN/1.0.0/pipelines
 mkdir test_pipeline
+
+# create text file to record scores and timing information
+touch scores.txt
+echo "DATASET, F1 SCORE, EXECUTION TIME" > scores.txt
 cd test_pipeline
 for i in "${Datasets[@]}"; do
-  python3 "LSTM_FCN_pipeline_$i.py" $i 
-  python3 -m d3m runtime -d /datasets/ fit-score -m *.meta -p *.json
-  # logic to write score and timing information to a separate file
+
+  # generate and save pipeline + metafile
+  python3 "LSTM_FCN_pipeline_$i.py" $i
   cp * ../
-  rm *
+
+  # test and score pipeline
+  start=`date +%s` 
+  python3 -m d3m runtime -d /datasets/ fit-score -m *.meta -p *.json -c scores.csv
+  end=`date +%s`
+  runtime=$((end-start))
+
+  # save information
+  echo "\n$i, " > scores.txt
+  IFS=, read col1 score col3 col4 < <(tail -n1 scores.csv)
+  echo "$score, $runtime" > scores.txt
+  
+  # cleanup temporary file
+  rm *.meta
+  rm *.json
+  rm scores.csv
 done
