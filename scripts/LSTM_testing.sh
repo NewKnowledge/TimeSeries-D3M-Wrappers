c62111a853e9d14aecd414b093c44fd976a13da4
jgleason <jeffrey.gleason@newknowledge.io>
2019-06-12 19:42:35 +0000

    latest LSTM bash script

--- scripts/LSTM_testing.sh
+++ scripts/LSTM_testing.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -e 
 
-Datasets=('LL1_Adiac')
-# 'LL1_ArrowHead' '66_chlorineConcentration' 'LL1_CinC_ECG_torso' 'LL1_Cricket_Y' 'LL1_ECG200' 'LL1_ElectricDevices' 'LL1_FISH' 'LL1_FaceFour' 'LL1_FordA' 'LL1_HandOutlines' 'LL1_Haptics' 'LL1_ItalyPowerDemand' 'LL1_Meat' 'LL1_OSULeaf')
+Datasets=('LL1_Adiac' 'LL1_ArrowHead' '66_chlorineConcentration' 'LL1_CinC_ECG_torso' 'LL1_Cricket_Y' 'LL1_ECG200' 'LL1_ElectricDevices' 'LL1_FISH' 'LL1_FaceFour' 'LL1_FordA' 'LL1_HandOutlines' 'LL1_Haptics' 'LL1_ItalyPowerDemand' 'LL1_Meat' 'LL1_OSULeaf')
 cd /primitives/v2019.6.7/Distil/d3m.primitives.time_series_classification.convolutional_neural_net.LSTM_FCN/1.0.0/pipelines
-mkdir test_pipeline
+#mkdir test_pipeline
 cd test_pipeline
 
 # create text file to record scores and timing information
@@ -13,7 +12,7 @@ echo "DATASET, F1 SCORE, EXECUTION TIME" >> scores.txt
 for i in "${Datasets[@]}"; do
 
   # generate and save pipeline + metafile
-  python3 "../LSTM_FCN_pipeline_$i.py" $i
+  python3 "/src/timeseriesd3mwrappers/TimeSeriesD3MWrappers/pipelines/LSTM_FCN_pipeline_$i.py" $i
 
   # test and score pipeline
   start=`date +%s` 
