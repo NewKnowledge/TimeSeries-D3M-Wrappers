9dc7896d0e6c38545fa308a616e41b5529adad88
jgleason <jeffrey.gleason.3@gmail.com>
2019-06-13 21:45:44 +0000

    correct pipeline typo

--- TimeSeriesD3MWrappers/pipelines/Hdbscan_pipeline.py
+++ TimeSeriesD3MWrappers/pipelines/Hdbscan_pipeline.py
@@ -9,13 +9,13 @@ pipeline_description.add_input(name='inputs')
 # Step 0: Denormalize primitive
 step_0 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.denormalize.Common'))
 step_0.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='inputs.0')
-step_0.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='inputs.0')
 step_0.add_output('produce')
 pipeline_description.add_step(step_0)
 
 # Step 1: DISTIL/NK Storc primitive
 step_1 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.clustering.hdbscan.Hdbscan'))
 step_1.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
+step_1.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
 step_1.add_output('produce')
 pipeline_description.add_step(step_1)
 
