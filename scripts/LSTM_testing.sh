2b9afc1162f1d55d42c4cbf7c528dc6008f7e9e7
jgleason <jeffrey.gleason@newknowledge.io>
2019-06-13 17:30:24 +0000

    latest version of LSTM bash script

--- scripts/LSTM_testing.sh
+++ scripts/LSTM_testing.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e 
 
 Datasets=('LL1_Adiac' 'LL1_ArrowHead' '66_chlorineConcentration' 'LL1_CinC_ECG_torso' 'LL1_Cricket_Y' 'LL1_ECG200' 'LL1_ElectricDevices' 'LL1_FISH' 'LL1_FaceFour' 'LL1_FordA' 'LL1_HandOutlines' 'LL1_Haptics' 'LL1_ItalyPowerDemand' 'LL1_Meat' 'LL1_OSULeaf')
+mkdir /primitives/v2019.6.7/Distil/d3m.primitives.time_series_classification.convolutional_neural_net.LSTM_FCN
+mkdir /primitives/v2019.6.7/Distil/d3m.primitives.time_series_classification.convolutional_neural_net.LSTM_FCN/1.0.0
+mkdir /primitives/v2019.6.7/Distil/d3m.primitives.time_series_classification.convolutional_neural_net.LSTM_FCN/1.0.0/pipelines
 cd /primitives/v2019.6.7/Distil/d3m.primitives.time_series_classification.convolutional_neural_net.LSTM_FCN/1.0.0/pipelines
-#mkdir test_pipeline
+mkdir test_pipeline
 cd test_pipeline
 
 # create text file to record scores and timing information
