7f6bda5b92bd68a84c2f60c76222daf9b0fac690
jgleason <jeffrey.gleason@newknowledge.io>
2019-06-13 17:29:06 +0000

    latest docker setup script

--- scripts/docker_setup.sh
+++ scripts/docker_setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
+cd /
 git clone https://gitlab.com/jgleason/primitives
 cd primitives
-git checkout jg/v5.8
+git branch lstm_pipelines
+git checkout lstm_pipelines
 git remote add upstream https://gitlab.com/datadrivendiscovery/primitives
 git pull upstream master
