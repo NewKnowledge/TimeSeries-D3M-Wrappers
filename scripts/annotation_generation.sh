7ce1dcf2b7522599813b4cfdcd4ce7769fec6106
jgleason <jeffrey.gleason.3@gmail.com>
2019-06-16 19:47:14 +0000

    add new scripts and python pipelines

--- scripts/annotation_generation.sh
+++ scripts/annotation_generation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-Paths=('d3m.primitives.clustering.hdbscan.Hdbscan' 'd3m.primitives.clustering.k_means.Sloth' 'd3m.primitives.time_series_classification.k_neighbors.Kanine' 'd3m.primitives.time_series_classification.shapelet_learning.Shallot' 'd3m.primitives.time_series_forecasting.arima.Parrot' 'd3m.primitives.time_series_forecasting.vector_autoregression.VAR')
-cd /primitives/v2019.6.7/Distil
+Paths=('d3m.primitives.clustering.hdbscan.Hdbscan' 'd3m.primitives.clustering.k_means.Sloth' 'd3m.primitives.time_series_classification.k_neighbors.Kanine' 'd3m.primitives.time_series_classification.shapelet_learning.Shallot' 'd3m.primitives.time_series_forecasting.arima.Parrot' 'd3m.primitives.time_series_forecasting.vector_autoregression.VAR' 'd3m.primitives.time_series_classification.convolutional_neural_net.LSTM_FCN')
 for i in "${Paths[@]}"; do
+  cd /primitives/v2019.6.7/Distil
   cd $i
   echo $i
   cd *
