15bc06acb685f3a6fadea42ab68b929ceb466d5c
jgleason <jeffrey.gleason@newknowledge.io>
2019-06-19 14:43:47 -0500

    update arrowhead pipeline

--- TimeSeriesD3MWrappers/pipelines/Sloth_pipeline_LL1_ArrowHead.py
+++ TimeSeriesD3MWrappers/pipelines/Sloth_pipeline_LL1_ArrowHead.py
@@ -20,13 +20,13 @@ step_1.add_hyperparameter(name='nclusters', argument_type= ArgumentType.VALUE, d
 step_1.add_output('produce')
 pipeline_description.add_step(step_1)
 
-# Step 3: column_parser
+# Step 2: column_parser
 step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.column_parser.DataFrameCommon'))
 step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
 step_2.add_output('produce')
 pipeline_description.add_step(step_2)
 
-# Step 4: imputer
+# Step 3: imputer
 step_3 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_cleaning.imputer.SKlearn'))
 step_3.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
 step_3.add_output('produce')
@@ -34,7 +34,7 @@ step_3.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE
 step_3.add_hyperparameter(name='use_semantic_types', argument_type=ArgumentType.VALUE,data=True)
 pipeline_description.add_step(step_3)
 
-# Step 2: Gradient boosting classifier
+# Step 4: Gradient boosting classifier
 step_4 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.classification.xgboost_gbtree.DataFrameCommon'))
 step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
 step_4.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
