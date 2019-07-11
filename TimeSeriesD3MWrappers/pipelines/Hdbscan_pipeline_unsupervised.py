82d5250503358567e6d137e051a53befba49e5f5
jgleason <jeffrey.gleason@newknowledge.io>
2019-07-11 14:24:54 -0700

    iterate on semi-supervised pipelines

--- TimeSeriesD3MWrappers/pipelines/Hdbscan_pipeline_unsupervised.py
+++ TimeSeriesD3MWrappers/pipelines/Hdbscan_pipeline_unsupervised.py
@@ -21,9 +21,13 @@ step_1.add_output('produce')
 pipeline_description.add_step(step_1)
 
 # Step 1: Map Sklearn imputer to dataset
+step_2a = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_cleaning.imputer.SKlearn'))
+step_2a.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE,data='replace')
+step_2a.add_hyperparameter(name='use_semantic_types', argument_type=ArgumentType.VALUE,data=True)
+step_2a.add_output('produce')
 step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.operator.dataset_map.DataFrameCommon'))
 step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
-step_2.add_hyperparameter(name='primitive', argument_type= ArgumentType.VALUE, data=index.get_primitive('d3m.primitives.data_cleaning.imputer.SKlearn'))
+step_2.add_hyperparameter(name='primitive', argument_type= ArgumentType.CONTAINER, data='steps.2a.produce'))
 step_2.add_output('produce')
 pipeline_description.add_step(step_2)
 
