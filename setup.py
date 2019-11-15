b0d4c88fc99f2881a69e70c14cf8439a901fbc31
jgleason <jeffrey.gleason@newknowledge.io>
2019-11-15 14:38:33 -0600

    testing new KNN pipeline

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(name='TimeSeriesD3MWrappers',
             'time_series_classification.k_neighbors.Kanine = TimeSeriesD3MWrappers:Kanine',
             'time_series_forecasting.arima.Parrot = TimeSeriesD3MWrappers:Parrot',
             'time_series_forecasting.vector_autoregression.VAR = TimeSeriesD3MWrappers:VAR',
-            'time_series_classification.convolutional_neural_net.LSTM_FCN = TimeSeriesD3MWrappers:LSTM_FCN'
+            'time_series_classification.convolutional_neural_net.LSTM_FCN = TimeSeriesD3MWrappers:LSTM_FCN',
+            'data_transformation.data_cleaning.DistilTimeSeriesFormatter = TimeSeriesD3MWrappers:TimeSeriesFormatterPrimitive'
         ],
     },
 )
